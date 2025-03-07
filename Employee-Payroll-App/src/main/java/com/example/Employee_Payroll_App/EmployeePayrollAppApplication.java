c875d8d70b4ec321d6a669f4c82a3e6f35074448
Rahuljn2004 <rahul.jaincs21@gmail.com>
2025-03-07 11:28:52 +0530

    Setup project

---
+++ Employee-Payroll-App/src/main/java/com/example/Employee_Payroll_App/EmployeePayrollAppApplication.java
@@ -0,0 +1,14 @@
+package com.example.Employee_Payroll_App;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeePayrollAppApplication {
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(EmployeePayrollAppApplication.class, args);
+	}
+
+}
